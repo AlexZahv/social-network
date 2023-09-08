e2414707fdd70214d3d3dafb75d950ede1c63e36
Zakhvalinskiy.A.V <Zakhvalinskiy.A.V@sblogistica.ru>
2023-09-08 21:10:11 +0300

    Homework-8 - dialogs moved to redis

--- src/main/kotlin/ru/zahv/alex/socialnetwork/business/persistance/domain/DialogMessageEntity.kt
+++ src/main/kotlin/ru/zahv/alex/socialnetwork/business/persistance/domain/DialogMessageEntity.kt
@@ -1,12 +1,10 @@
 package ru.zahv.alex.socialnetwork.business.persistance.domain
 
-import java.time.LocalDateTime
-
-data class DialogMessageEntity (
-    var id: String? = null,
-    var dialogId: String? = null,
-    var text: String? = null,
-    var fromUserId: String? = null,
-    var toUserId: String? = null,
-    var createDate: LocalDateTime? = null
+data class DialogMessageEntity(
+        var id: String? = null,
+        var dialogId: String? = null,
+        var text: String? = null,
+        var fromUserId: String? = null,
+        var toUserId: String? = null,
+        var createDate: String? = null
 )
